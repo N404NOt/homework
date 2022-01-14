6c24fe3149f7637cc36255bd2260cb64880707de
N404NOt <97644912+N404NOt@users.noreply.github.com>
2022-01-14 10:18:58 +0800

    Add files via upload

---
+++ 输出图案EOF/源.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a;
+	while (scanf("%d", &a)!=EOF);
+	{
+		for (int i = 0; i++; i++)
+		{
+			printf("*");
+
+		}
+		printf("\n");
+	}
+}
